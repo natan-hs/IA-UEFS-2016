4985863f119a8888c4bc9279bdd77a1dc05479e1
Natan <natan.ecomp@gmail.com>
2016-06-01 00:28:16 -0300

    Primeiro commit com as coisas desenvolvidas na aula do dia 1-06-2016

---
+++ KeystrokeLogin/src/keystrokelogin/senha.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package keystrokelogin;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author aluno
+ */
+public class senha {
+    private String nome;
+    private long[] tempos;
+    private boolean result;
+    
+    public senha (String nome, long[] tempos, boolean result){
+        this.nome = nome;
+        this.tempos = tempos;
+        this.result = result;
+    }
+    
+}
